39d673d7b2a6e20a2d30892dde4f795f90557445
Chu Shao <cshao@churenshao.com>
2013-11-24 15:15:30 -0800

    Added Kinect Gesture files

---
+++ Fizbin.Kinect.Gestures-master/Fizbin.Kinect.Gestures-master/Fizbin.Kinect.Gestures.Demo/MainWindow.xaml.cs
@@ -0,0 +1,426 @@
+﻿using System.Windows;
+using System.Windows.Data;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Samples.Kinect.WpfViewers;
+using System.ComponentModel;
+using System;
+using System.Timers;
+using Fizbin.Kinect.Gestures.Segments;
+using Newtonsoft.Json;
+using SharpVoice;
+using System.IO.Ports;
+
+
+
+
+namespace Fizbin.Kinect.Gestures.Demo
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private readonly KinectSensorChooser sensorChooser = new KinectSensorChooser();
+
+        private Skeleton[] skeletons = new Skeleton[0];
+
+        private int LIGHT = 97;           // [97 - 106]
+        private int CURTAIN = 107;        // [107 - 116]
+        private const int TV_ON = 118;
+        private const int TV_OFF = 117;
+        private const int CHANNEL_UP = 119;
+        private const int CHANNEL_DOWN = 120;
+        private const int VOLUME_UP = 121;
+        private const int VOLUME_DOWN = 122;
+        private const int APPLIANCE_OFF = 65;
+        private const int APPLIANCE_ON = 66;
+
+        // Flags to check for device status
+        private bool tv_on = false;
+        private bool appliance_on = false;
+        
+        Timer _clearTimer;
+        SerialPort serialPort;
+
+
+        // skeleton gesture recognizer
+        private GestureController gestureController;
+
+        public MainWindow()
+        {
+            Console.WriteLine("MainWindow()");
+            DataContext = this;
+
+            InitializeComponent();
+
+            // initialize the Kinect sensor manager
+            KinectSensorManager = new KinectSensorManager();
+            KinectSensorManager.KinectSensorChanged += this.KinectSensorChanged;
+
+            // locate an available sensor
+            sensorChooser.Start();
+
+            // bind chooser's sensor value to the local sensor manager
+            var kinectSensorBinding = new Binding("Kinect") { Source = this.sensorChooser };
+            BindingOperations.SetBinding(this.KinectSensorManager, KinectSensorManager.KinectSensorProperty, kinectSensorBinding);
+
+			// add timer for clearing last detected gesture
+            _clearTimer = new Timer(2000);
+            _clearTimer.Elapsed += new ElapsedEventHandler(clearTimer_Elapsed);
+
+            // add SerialPort for sending messages to Arduino
+            serialPort = new SerialPort("COM1", 9600);
+        }
+
+        #region Kinect Discovery & Setup
+
+        private void KinectSensorChanged(object sender, KinectSensorManagerEventArgs<KinectSensor> args)
+        {
+            if (null != args.OldValue)
+                UninitializeKinectServices(args.OldValue);
+
+            if (null != args.NewValue)
+                InitializeKinectServices(KinectSensorManager, args.NewValue);
+        }
+
+        /// <summary>
+        /// Kinect enabled apps should customize which Kinect services it initializes here.
+        /// </summary>
+        /// <param name="kinectSensorManager"></param>
+        /// <param name="sensor"></param>
+        private void InitializeKinectServices(KinectSensorManager kinectSensorManager, KinectSensor sensor)
+        {
+            // Application should enable all streams first.
+
+            // configure the color stream
+            kinectSensorManager.ColorFormat = ColorImageFormat.RgbResolution640x480Fps30;
+            kinectSensorManager.ColorStreamEnabled = true;
+
+            // configure the depth stream
+            kinectSensorManager.DepthStreamEnabled = true;
+
+            kinectSensorManager.TransformSmoothParameters =
+                new TransformSmoothParameters
+                {
+                    Smoothing = 0.5f,
+                    Correction = 0.5f,
+                    Prediction = 0.5f,
+                    JitterRadius = 0.05f,
+                    MaxDeviationRadius = 0.04f
+                };
+
+            // configure the skeleton stream
+            sensor.SkeletonFrameReady += OnSkeletonFrameReady;
+            kinectSensorManager.SkeletonStreamEnabled = true;
+
+            kinectSensorManager.KinectSensorEnabled = true;
+
+            if (!kinectSensorManager.KinectSensorAppConflict)
+            {
+                // initialize the gesture recognizer
+                gestureController = new GestureController();
+                gestureController.GestureRecognized += OnGestureRecognized;
+
+                // register the gestures for this demo
+                RegisterGestures();
+            }
+        }
+
+        /// <summary>
+        /// Kinect enabled apps should uninitialize all Kinect services that were initialized in InitializeKinectServices() here.
+        /// </summary>
+        /// <param name="sensor"></param>
+        private void UninitializeKinectServices(KinectSensor sensor)
+        {
+            // unregister the event handlers
+            sensor.SkeletonFrameReady -= OnSkeletonFrameReady;
+            gestureController.GestureRecognized -= OnGestureRecognized;
+        }
+
+        #endregion Kinect Discovery & Setup
+
+        /// <summary>
+        /// Helper function to register all available 
+        /// </summary>
+        private void RegisterGestures()
+        {
+            // define the gestures for the demo
+            Console.WriteLine("RegisterGestures()");
+            IRelativeGestureSegment[] joinedhandsSegments = new IRelativeGestureSegment[20];
+            JoinedHandsSegment1 joinedhandsSegment = new JoinedHandsSegment1();
+            for (int i = 0; i < 20; i++)
+            {
+                // gesture consists of the same thing 10 times 
+                joinedhandsSegments[i] = joinedhandsSegment;
+            }
+            gestureController.AddGesture("JoinedHands", joinedhandsSegments);
+
+            IRelativeGestureSegment[] menuSegments = new IRelativeGestureSegment[20];
+            MenuSegment1 menuSegment = new MenuSegment1();
+            for (int i = 0; i < 20; i++)
+            {
+                // gesture consists of the same thing 20 times 
+                menuSegments[i] = menuSegment;
+            }
+            gestureController.AddGesture("Menu", menuSegments);
+
+            IRelativeGestureSegment[] swipeleftSegments = new IRelativeGestureSegment[3];
+            swipeleftSegments[0] = new SwipeLeftSegment1();
+            swipeleftSegments[1] = new SwipeLeftSegment2();
+            swipeleftSegments[2] = new SwipeLeftSegment3();
+            gestureController.AddGesture("SwipeLeft", swipeleftSegments);
+
+            IRelativeGestureSegment[] swiperightSegments = new IRelativeGestureSegment[3];
+            swiperightSegments[0] = new SwipeRightSegment1();
+            swiperightSegments[1] = new SwipeRightSegment2();
+            swiperightSegments[2] = new SwipeRightSegment3();
+            gestureController.AddGesture("SwipeRight", swiperightSegments);
+
+            IRelativeGestureSegment[] waveRightSegments = new IRelativeGestureSegment[6];
+            WaveRightSegment1 waveRightSegment1 = new WaveRightSegment1();
+            WaveRightSegment2 waveRightSegment2 = new WaveRightSegment2();
+            waveRightSegments[0] = waveRightSegment1;
+            waveRightSegments[1] = waveRightSegment2;
+            waveRightSegments[2] = waveRightSegment1;
+            waveRightSegments[3] = waveRightSegment2;
+            waveRightSegments[4] = waveRightSegment1;
+            waveRightSegments[5] = waveRightSegment2;
+            gestureController.AddGesture("WaveRight", waveRightSegments);
+
+            IRelativeGestureSegment[] waveLeftSegments = new IRelativeGestureSegment[6];
+            WaveLeftSegment1 waveLeftSegment1 = new WaveLeftSegment1();
+            WaveLeftSegment2 waveLeftSegment2 = new WaveLeftSegment2();
+            waveLeftSegments[0] = waveLeftSegment1;
+            waveLeftSegments[1] = waveLeftSegment2;
+            waveLeftSegments[2] = waveLeftSegment1;
+            waveLeftSegments[3] = waveLeftSegment2;
+            waveLeftSegments[4] = waveLeftSegment1;
+            waveLeftSegments[5] = waveLeftSegment2;
+            gestureController.AddGesture("WaveLeft", waveLeftSegments);
+
+            IRelativeGestureSegment[] zoomInSegments = new IRelativeGestureSegment[3];
+            zoomInSegments[0] = new ZoomSegment1();
+            zoomInSegments[1] = new ZoomSegment2();
+            zoomInSegments[2] = new ZoomSegment3();
+            gestureController.AddGesture("ZoomIn", zoomInSegments);
+
+            IRelativeGestureSegment[] zoomOutSegments = new IRelativeGestureSegment[3];
+            zoomOutSegments[0] = new ZoomSegment3();
+            zoomOutSegments[1] = new ZoomSegment2();
+            zoomOutSegments[2] = new ZoomSegment1();
+            gestureController.AddGesture("ZoomOut", zoomOutSegments);
+            
+            IRelativeGestureSegment[] swipeUpSegments = new IRelativeGestureSegment[3];
+            swipeUpSegments[0] = new SwipeUpSegment1();
+            swipeUpSegments[1] = new SwipeUpSegment2();
+            swipeUpSegments[2] = new SwipeUpSegment3();
+            gestureController.AddGesture("SwipeUp", swipeUpSegments);
+
+            IRelativeGestureSegment[] swipeDownSegments = new IRelativeGestureSegment[3];
+            swipeDownSegments[0] = new SwipeDownSegment1();
+            swipeDownSegments[1] = new SwipeDownSegment2();
+            swipeDownSegments[2] = new SwipeDownSegment3();
+            gestureController.AddGesture("SwipeDown", swipeDownSegments);
+            
+            IRelativeGestureSegment[] raisetheRoofSegments = new IRelativeGestureSegment[4];
+            raisetheRoofSegments[0] = new RaiseTheRoofSegment1();
+            raisetheRoofSegments[1] = new RaiseTheRoofSegment2();
+            raisetheRoofSegments[2] = new RaiseTheRoofSegment1();
+            raisetheRoofSegments[3] = new RaiseTheRoofSegment2();
+            gestureController.AddGesture("RaiseTheRoof", raisetheRoofSegments);
+
+        }
+
+        #region Properties
+
+        public static readonly DependencyProperty KinectSensorManagerProperty =
+            DependencyProperty.Register(
+                "KinectSensorManager",
+                typeof(KinectSensorManager),
+                typeof(MainWindow),
+                new PropertyMetadata(null));
+
+        public KinectSensorManager KinectSensorManager
+        {
+            get { return (KinectSensorManager)GetValue(KinectSensorManagerProperty); }
+            set { SetValue(KinectSensorManagerProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the last recognized gesture.
+        /// </summary>
+        private string _gesture;
+        public String Gesture
+        {
+            get { return _gesture; }
+
+            private set
+            {
+                if (_gesture == value)
+                    return;
+
+                _gesture = value;
+
+                if (this.PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Gesture"));
+            }
+        }
+
+        #endregion Properties
+
+        #region Events
+
+        /// <summary>
+        /// Event implementing INotifyPropertyChanged interface.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+
+        #region Event Handlers
+
+        private void OnRaiseTheRoof()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\Derek\Desktop\Fizbin.Kinect.Gestures-master\Fizbin.Kinect.Gestures-master\Fizbin.Kinect.Gestures.Demo\Pikachu.wav");
+            
+            player.Play();
+            CallSomeone();
+        }
+
+        private void CallSomeone()
+        {
+            Voice v = new Voice("koaswift@gmail.com", "cse118team8");
+            v.Call("+19255481182", "koaswift@gmail.com");               // Call from Google Voice Account -> Gmail account (Must have window open)
+            v.SendSMS("+19255481182", "Help I have fallen, and I cannot get up!\n\n # Test of Kinect Project - Kirsten's Phone");
+            v.SendSMS("+17027684490", "Help I have fallen, and I cannot get up!\n\n # Test of Kinect Project - Derek's Phone");
+
+        }
+
+        private void SendCommandToArduino(int msg)
+        {
+            serialPort.Write(""+(char)msg);   
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">Gesture event arguments.</param>
+        private void OnGestureRecognized(object sender, GestureEventArgs e)
+        {
+            switch (e.GestureName)
+            {
+                case "RaiseTheRoof":
+                    Gesture = "Raise The Roof!";
+                    this.OnRaiseTheRoof();
+                    break;
+                case "Menu":
+                    Gesture = "Menu";
+                    break;
+                case "WaveRight":
+                    Gesture = "Wave Right";
+                    break;
+                case "WaveLeft":
+                    Gesture = "Wave Left";
+                    break;
+                case "JoinedHands":
+                    Gesture = "Joined Hands";
+                    break;
+                case "SwipeLeft":
+                    Gesture = "Swipe Left";
+                    if (tv_on)
+                    {
+                        this.SendCommandToArduino(VOLUME_UP);
+                    }
+                    break;
+                case "SwipeRight":
+                    Gesture = "Swipe Right";
+                    if (tv_on)
+                    {
+                        this.SendCommandToArduino(VOLUME_DOWN);
+                    }
+ 					break;
+                case "SwipeUp":
+                    Gesture = "Swipe Up";
+                    if (tv_on)
+                    {
+                        this.SendCommandToArduino(CHANNEL_DOWN);
+                    }
+                    break;
+                case "SwipeDown":
+                    Gesture = "Swipe Down";
+                    if (tv_on)
+                    {
+                        this.SendCommandToArduino(CHANNEL_UP);
+                    }
+                    break;
+                case "ZoomIn":
+                    Gesture = "Zoom In";
+                    if (!tv_on)
+                    {
+                        this.SendCommandToArduino(TV_ON);
+                    }
+                    break;
+                case "ZoomOut":
+                    Gesture = "Zoom Out";
+                    if (tv_on)
+                    {
+                        this.SendCommandToArduino(TV_OFF);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("HALLO " + Gesture);
+            _clearTimer.Start();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame frame = e.OpenSkeletonFrame())
+            {
+                if (frame == null)
+                    return;
+
+                // resize the skeletons array if needed
+                if (skeletons.Length != frame.SkeletonArrayLength)
+                    skeletons = new Skeleton[frame.SkeletonArrayLength];
+
+                // get the skeleton data
+                frame.CopySkeletonDataTo(skeletons);
+
+                foreach (var skeleton in skeletons)
+                {
+                    // skip the skeleton if it is not being tracked
+                    if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+                        continue;
+
+                    // update the gesture controller
+                    gestureController.UpdateAllGestures(skeleton);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clear text after some time
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void clearTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Gesture = "";
+            _clearTimer.Stop();
+        }
+
+        #endregion Event Handlers
+
+    }
+}
---
+++ Fizbin.Kinect.Gestures-master/Fizbin.Kinect.Gestures-master/Fizbin.Kinect.Gestures/Segments/RaiseTheRoofSegment.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Kinect;
+
+namespace Fizbin.Kinect.Gestures.Segments
+{
+    public class RaiseTheRoofSegment1 : IRelativeGestureSegment
+    {
+         /// <summary>
+        /// Checks the gesture.
+        /// </summary>
+        /// <param name="skeleton">The skeleton.</param>
+        /// <returns>GesturePartResult based on if the gesture part has been completed</returns>
+        public GesturePartResult CheckGesture(Skeleton skeleton)
+        {
+            // Left and right hand are higher than shoulders
+            if (skeleton.Joints[JointType.HandLeft].Position.Y >= skeleton.Joints[JointType.ShoulderLeft].Position.Y
+                && skeleton.Joints[JointType.HandRight].Position.Y >= skeleton.Joints[JointType.ShoulderRight].Position.Y)
+            {
+                // left and right elbow are less than shoulders
+               if (skeleton.Joints[JointType.ElbowLeft].Position.Y < skeleton.Joints[JointType.ShoulderLeft].Position.Y
+                   && skeleton.Joints[JointType.ElbowRight].Position.Y < skeleton.Joints[JointType.ShoulderRight].Position.Y)
+               {
+                    // left hand 0.2 to left of left elbow
+                 //   if (skeleton.Joints[JointType.HandLeft].Position.X < skeleton.Joints[JointType.ElbowLeft].Position.X - 0.2)
+                  //  {
+                        return GesturePartResult.Succeed;
+               }
+
+                return GesturePartResult.Pausing;
+            }
+
+            return GesturePartResult.Fail;
+        }
+    }
+
+    public class RaiseTheRoofSegment2 : IRelativeGestureSegment
+    {
+        /// <summary>
+        /// Checks the gesture.
+        /// </summary>
+        /// <param name="skeleton">The skeleton.</param>
+        /// <returns>GesturePartResult based on if the gesture part has been completed</returns>
+        public GesturePartResult CheckGesture(Skeleton skeleton)
+        {
+            // Left hand is higher than left shoulder 
+            if (skeleton.Joints[JointType.HandLeft].Position.Y >= skeleton.Joints[JointType.ShoulderLeft].Position.Y
+                && skeleton.Joints[JointType.HandRight].Position.Y >= skeleton.Joints[JointType.ShoulderRight].Position.Y)
+            {
+                // left elbow is above left shoulder
+                if (skeleton.Joints[JointType.ElbowLeft].Position.Y >= skeleton.Joints[JointType.ShoulderLeft].Position.Y
+                    && skeleton.Joints[JointType.ElbowRight].Position.Y >= skeleton.Joints[JointType.ShoulderRight].Position.Y)
+                {
+                    // left hand 0.2 to left of left elbow
+                    //   if (skeleton.Joints[JointType.HandLeft].Position.X < skeleton.Joints[JointType.ElbowLeft].Position.X - 0.2)
+                    //  {
+                    return GesturePartResult.Succeed;
+                }
+
+                return GesturePartResult.Pausing;
+            }
+
+            return GesturePartResult.Fail;
+        }
+    }
+}
